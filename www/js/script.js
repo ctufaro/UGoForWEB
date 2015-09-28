ec24fd24ce4671bd36757d55e0df5ac92ef9e7a3
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-09-28 16:38:42 -0400

    Implementing fastclick

--- www/js/script.js
+++ www/js/script.js
@@ -9,6 +9,10 @@ var Page = (function () {
 
     var init = function () {
 
+        //fastclick load
+        $(function () { FastClick.attach(document.body); });
+
+        //hashchange event
         $(window).on('hashchange', function () {
             render(window.location.hash);
         });
@@ -16,7 +20,7 @@ var Page = (function () {
         // Render default page
         renderSignOrLogPage();
 
-        // Redner UI Events
+        // Render UI Events
         renderUIEvents();
     }
 
@@ -218,27 +222,16 @@ var Page = (function () {
             console.log("loaded");
         });
 
-        $("#signUpNewUser").bind('touchstart', function () {
+        $("#signUpNewUser").click(function () {
             $('#frmSignUp').on('valid.fndtn.abide', function () {
-                try {
+                try{
                     PGPlugins.imageUpload();
                 }
                 catch (err) {
-                    alert("Oh Snap! " + err.message);
+                    alert("Oh Snap! " +err.message);
                 }
             });
-        })
-
-        //$("#signUpNewUser").click(function () {
-        //    $('#frmSignUp').on('valid.fndtn.abide', function () {
-        //        try{
-        //            PGPlugins.imageUpload();
-        //        }
-        //        catch (err) {
-        //            alert("Oh Snap! " +err.message);
-        //        }
-        //    });
-        //});
+        });
     };
 
     return { init: init, renderSpinner: renderSpinner, renderMainScreenPage: renderMainScreenPage, initUser: initUser, clearCache: clearCache };
