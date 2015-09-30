989e5db31450ea99fdc2ea15906e2b2f1b241b8e
christufaro <ctufaro@gmail.com>
2015-09-29 21:45:58 -0400

    Cleaning up shop

--- www/js/script.js
+++ www/js/script.js
@@ -164,24 +164,20 @@ var Page = (function () {
         var appendMe = "<article id='post'><img class='avatar' style='float: left' src='{profileurl}'><div class='avatar-profilename'>{profilename}</div><div class='arrow_box'>{littlecomment}<span class='day pull-right'>{day}</span> </div> <img class='cover' src='{foodurl}'> <div class='big-comment'> <p>{bigcomment}</p> <select class='post-icons' style='display: none;'> <option value='1'>Share</option> <option value='2'>Sweet</option> <option value='3'>Heart</option> </select><ul class='touchMultiSelect pull-right'><li class='noneButton '>None</li><li class='icon-share'>Share</li><li class='icon-sweet'>Sweet</li><li class='icon-heart'>Heart</li></ul> </div> </article>";
         var postsNonPure = "<article id='post'><img class='avatar' style='float: left' src=''><div class='avatar-profilename'></div><div class='arrow_box'><span class='day pull-right'></span> </div> <img class='cover' src=''> <div class='big-comment'> <p></p> <select class='post-icons' style='display: none;'> <option value='1'>Share</option> <option value='2'>Sweet</option> <option value='3'>Heart</option> </select><ul class='touchMultiSelect pull-right'><li class='noneButton '>None</li><li class='icon-share'>Share</li><li class='icon-sweet'>Sweet</li><li class='icon-heart'>Heart</li></ul> </div> </article>";
 
-        $("#profileImage").click(function () {
-            navigator.notification.confirm(
-                'from the following:', 
-                PGPlugins.confirmPhoto,           
-                'Select A Profile Image',
-                ['Photo Gallery', 'Take a Photo', 'Cancel']        
-            );
-        });
-
-        $("#mainLogomage").click(function () {
-            clearCache();
-        });
-
         $('.popup-modal').magnificPopup({type: 'inline',preloader: false,focus: '#username', modal: true});
 
         $(".popup-modal-dismiss").click(function (e) {
             e.preventDefault();
             $.magnificPopup.close();
+        });
+
+        $("#profileImage").click(function () {
+            navigator.notification.confirm(
+                'from the following:',
+                PGPlugins.confirmPhoto,
+                'Select A Profile Image',
+                ['Photo Gallery', 'Take a Photo', 'Cancel']
+            );
         });
 
         $("#btnPost").click(function () {
@@ -223,12 +219,16 @@ var Page = (function () {
             })
         });
 
-        $("#btnRefresh").click(function () {
+        $("#btnSettingRefresh").click(function () {
             $(".posts").css("display", "none");
             $(".posts").html(postsNonPure);
             loaded = false;
             renderMainScreenPage();
             console.log("loaded");
+        });
+
+        $("#btnSettingCache").click(function () {
+            clearCache();
         });
 
         $("#signUpNewUser").click(function () {
