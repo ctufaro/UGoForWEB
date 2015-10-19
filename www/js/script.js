d8e455721277b82a54f7f150bdfd0609f4d633d3
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-10-19 14:50:52 -0400

    Reading JSON comments values

--- www/js/script.js
+++ www/js/script.js
@@ -320,7 +320,12 @@ var Feed = function () {
                             '.cover@src': 'post.PostedImage',
                             '.day': 'post.TimePosted',
                             '.big-comment p': 'post.BigComment',
-                            '.post-comments-msg': ' comments coming soon!!'
+                            '.post-comment': {
+                                'pc<-post.PostComments': {
+                                    '.post-comments-poster': 'pc.PostCommenter',
+                                    '.post-comments-msg': 'pc.PostComment'
+                                }
+                            }
                         }
                     }
                 };
@@ -667,8 +672,9 @@ var Message = function () {
     String Constants Class
 */
 var Constants = function () {
-    var PostHTML = "<article id='post'><img class='avatar' style='float: left' src='{profileurl}'><div class='avatar-profilename'>{profilename}</div><div class='arrow_box'>{littlecomment}<span class='day pull-right'>{day}</span> </div> <img class='cover' src='{foodurl}'> <div class='big-comment'> <p>{bigcomment}</p><span class='icon-heart' style='float:right;'></span><div class='post-comments' style='font-size:14px;'><div><span class='post-comments-poster avatar-profilename'></span><span class='post-comments-msg'></span></div></div></div></article>";
-    var PostPure = "<article id='post'><img class='avatar' style='float: left' src=''><div class='avatar-profilename'></div><div class='arrow_box'><span class='day pull-right'></span> </div> <img class='cover' src=''> <div class='big-comment'> <p></p> <span class='icon-heart' style='float:right;'></span><div class='post-comments' style='font-size:14px;'><div><span class='post-comments-poster avatar-profilename'></span><span class='post-comments-msg'></span></div></div></div></article>";
+    var PostCommentHTML = "<span class='icon-heart' style='float:right;'></span><div class='post-comments' style='font-size:14px;'><div class='post-comment'><span class='post-comments-poster avatar-profilename'></span><span class='post-comments-msg'></span></div></div>";
+    var PostHTML = "<article id='post'><img class='avatar' style='float: left' src='{profileurl}'><div class='avatar-profilename'>{profilename}</div><div class='arrow_box'>{littlecomment}<span class='day pull-right'>{day}</span> </div> <img class='cover' src='{foodurl}'> <div class='big-comment'> <p>{bigcomment}</p><span class='icon-heart' style='float:right;'></span>" + PostCommentHTML + "</div></article>";
+    var PostPure = "<article id='post'><img class='avatar' style='float: left' src=''><div class='avatar-profilename'></div><div class='arrow_box'><span class='day pull-right'></span> </div> <img class='cover' src=''> <div class='big-comment'> <p></p> <span class='icon-heart' style='float:right;'></span>" + PostCommentHTML + "</div></article>";
     //var RESTPosts = "http://192.168.1.2:26684/api/posts";
     //var RESTBlob = "http://192.168.1.2:26684/blobs/upload";
     var RESTPosts = "http://ugoforapi.azurewebsites.net/api/posts";
