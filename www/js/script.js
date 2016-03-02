fb9bde885e571f5fdd139b41e36737216f93fa0f
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2016-03-02 12:01:04 -0500

    Wiring up flags

--- www/js/script.js
+++ www/js/script.js
@@ -771,6 +771,9 @@ var Feed = function () {
         $(flagIcon).click(function (e) {
             var postid = $(this).data('postid');
             navigator.notification.confirm("Inappropriate content will be flagged and reviewed by administrators. Are you sure you want to flag this content as inappropriate?", function(buttonIndex){
+                alert('here');
+                alert(buttonIndex);
+                alert(buttonIndex === 1);
                 if (buttonIndex === 1) {
                     $(this).addClass('flag-red-icon-img');
                     FlagContent();
